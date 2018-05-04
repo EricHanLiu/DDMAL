2280ff10d3fdacd66998f451f2a4e44a1e671e4e
Eric Liu <ericliu404@gmail.com>
2018-05-04 10:40:23 -0400

    Fix: Change where Django serves files and where to fetch from

--- django-exercise/mysite/scores/models.py
+++ django-exercise/mysite/scores/models.py
@@ -3,5 +3,5 @@ from django.db import models
 
 class Scores(models.Model):
     composer = models.CharField(max_length=200)
-    scorePDF = models.FileField(upload_to='static/scores')
-    scoreMEI = models.FileField(upload_to='static/scores')
+    scorePDF = models.FileField()
+    scoreMEI = models.FileField()
